6b971f02296435aac341090250e25dc1841c3c41
areejhussein <114420621+areejhussein@users.noreply.github.com>
2023-09-25 16:52:56 +0300

    Initial commit

---
+++ InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Areej Hussein on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
